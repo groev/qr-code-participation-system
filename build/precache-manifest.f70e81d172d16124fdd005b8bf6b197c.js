2ca6b87f671ea0304e49665fac80073507e160bf
Magnus Westhofen <m.westhofen@12-05.de>
2020-05-23 21:21:33 +0200

    'commit'

---
+++ build/precache-manifest.f70e81d172d16124fdd005b8bf6b197c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff46afdc2a2da73fbeb028e44a600e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc78805e7de7a6e7b7b2",
+    "url": "/static/css/main.bf62c33d.chunk.css"
+  },
+  {
+    "revision": "c409dc9fd0d65df0a5f3",
+    "url": "/static/js/2.0402e4ad.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.0402e4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc78805e7de7a6e7b7b2",
+    "url": "/static/js/main.7b411784.chunk.js"
+  },
+  {
+    "revision": "68e970b3749b413aa81f",
+    "url": "/static/js/runtime-main.89f41c2f.js"
+  },
+  {
+    "revision": "279e71bae131b61cdfb55fd348fe6da8",
+    "url": "/static/media/close.279e71ba.svg"
+  },
+  {
+    "revision": "28c0416e95e15d05fb0fa31a06eee9fe",
+    "url": "/static/media/marker.28c0416e.svg"
+  },
+  {
+    "revision": "5b81a5ebebf20c2ae792d110500a20fc",
+    "url": "/static/media/menu.5b81a5eb.svg"
+  },
+  {
+    "revision": "9138e3d74c87d8e499cda0c026ef8589",
+    "url": "/static/media/more.9138e3d7.svg"
+  },
+  {
+    "revision": "8e43a4b49c79929ac185b0f82338992a",
+    "url": "/static/media/phone.8e43a4b4.svg"
+  },
+  {
+    "revision": "947d00bdf5a08e59bdbc69a4a36478db",
+    "url": "/static/media/remove.947d00bd.svg"
+  },
+  {
+    "revision": "e159645e09f1687bdcf256179caac643",
+    "url": "/static/media/scan.e159645e.svg"
+  },
+  {
+    "revision": "901a5971ad9cf74a961c496e43a94fcb",
+    "url": "/static/media/send.901a5971.svg"
+  }
+]);
