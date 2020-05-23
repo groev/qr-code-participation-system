46992d5692fcc61cef2822caf4a237a3526e8d8c
Magnus Westhofen <m.westhofen@12-05.de>
2020-05-23 20:56:17 +0200

    'commit'

--- src/components/pages/Scan.jsx
+++ src/components/pages/Scan.jsx
@@ -19,6 +19,7 @@ export default function Scan() {
   const [scanning, setScanning] = useState(false);
   const [customers, setCustomers] = useState([]);
   const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const customValues = JSON.parse(localStorage.getItem("customValues"));
     setValuesFromStorage(customValues);
