802914f80bce71fc087722849cd7eb74a3a44541
Magnus Westhofen <m.westhofen@12-05.de>
2020-05-24 19:37:18 +0200

    'commit'

--- build/precache-manifest.60348de39f3b62d184fbc47df3d04dad.js
+++ build/precache-manifest.60348de39f3b62d184fbc47df3d04dad.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce58dd5c9cf2f9268025e850c4974087",
+    "revision": "7e1f3940df9786c89144dc0003d1caf1",
     "url": "/index.html"
   },
   {
-    "revision": "566c793e5e098f4332b9",
+    "revision": "d9322f34cae4b02d3dae",
     "url": "/static/css/main.bf62c33d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0402e4ad.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "566c793e5e098f4332b9",
-    "url": "/static/js/main.0a7365ee.chunk.js"
+    "revision": "d9322f34cae4b02d3dae",
+    "url": "/static/js/main.5d1c8545.chunk.js"
   },
   {
     "revision": "68e970b3749b413aa81f",
